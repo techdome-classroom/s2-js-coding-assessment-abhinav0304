559e500c4b824e6656cefd75001e13ec367cf8d9
Avinash Kole <avinashkole@Avinashs-MacBook-Air.local>
2024-11-11 15:38:26 +0530

    Your commit message here

--- program1.js
+++ program1.js
@@ -3,7 +3,22 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    let stack = [];
+    for (let char of s) {
+        if (char === '(' || char === '{' || char === '[') {
+            stack.push(char);
+        }
+        else {
+            if (stack.length === 0) return false;
+            let last = stack.pop();
+            if ((char === ')' && last !== '(') || 
+                (char === '}' && last !== '{') || 
+                (char === ']' && last !== '[')) {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
--- program2.js
+++ program2.js
@@ -3,7 +3,28 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let total = 0;
+    for (let i = 0; i < s.length; i++) {
+        let currentValue = romanMap[s[i]];
+        let nextValue = romanMap[s[i + 1]];
+        if (currentValue < nextValue) {
+            total -= currentValue;
+        } else {
+            total += currentValue;
+        }
+    }
+
+    return total;
 };
 
 
